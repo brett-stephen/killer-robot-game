b5c02a2b2163b131ff64dca66431d8974a514bb7
Gideon Richter <gideonrich@hotmail.ca>
2019-03-15 13:45:31 -0600

    Refactor drawing

--- draw_robot.c
+++ draw_robot.c
@@ -24,6 +24,16 @@ void cb_keyboard(unsigned char key, int x, int y)
   }
 }
 
+void set_color_red() {glColor3f(1.0, 0.0, 0.0);}
+
+void set_color_green() {glColor3f(0.0, 1.0, 0.0);}
+
+void set_color_default() {glColor3f(0.0, 0.0, 0.0);}
+
+void set_color_black() {glColor3f(0.0, 0.0, 0.0);}
+
+void set_color_blue() {glColor3f(0.0, 0.0, 1.0);}
+
 void draw_triangle(double size)
 {
   glBegin(GL_TRIANGLES);
@@ -33,10 +43,112 @@ void draw_triangle(double size)
   glEnd();
 }
 
+void draw_body()
+{
+  set_color_red();
+  glPushMatrix();
+    glScalef(1.0, 1.5, 1.0);
+    glutSolidCube(1.0);
+  glPopMatrix();
+  set_color_default();
+}
+
+void draw_front_rectangle()
+{
+  glPushMatrix();
+    set_color_green();
+    glTranslatef(0.0, 0.0, 0.51);
+    glScalef(0.5, 1.0, 0.0);
+    glutSolidCube(1.0);
+    set_color_default();
+  glPopMatrix();
+}
+
+void draw_rear_triangles()
+{
+  glPushMatrix();
+    set_color_green();
+    glTranslatef(0.0, 0.0, -0.51);
+    glPushMatrix();
+      // Draw the upper triangle
+      glTranslatef(0.0, 0.0, 0.0);
+      draw_triangle(1.0);
+    glPopMatrix();
+    glPushMatrix();
+      // Draw the lower triangle
+      glTranslatef(0.0, -0.5, 0.0);
+      draw_triangle(1.0);
+    glPopMatrix();
+    set_color_default();
+  glPopMatrix();
+}
+
+void draw_neck()
+{
+  glPushMatrix();
+    set_color_green();
+    glTranslatef(0.0, 0.9, 0.0);
+    glRotatef(90, 1.0, 0.0, 0.0);
+    GLUquadricObj *quadratic;
+    quadratic = gluNewQuadric();
+    gluCylinder(quadratic, 0.3, 0.3, 0.2, 32, 32);
+    set_color_default();
+  glPopMatrix();
+}
+
+void draw_antenna()
+{
+  glPushMatrix();
+    set_color_black();
+    glTranslatef(0.0, 0.65, 0.0);
+    glRotatef(90, 1.0, 0.0, 0.0);
+    GLUquadricObj *quadratic;
+    quadratic = gluNewQuadric();
+    gluCylinder(quadratic, 0.1, 0.1, 0.4, 32, 32);
+    set_color_default();
+  glPopMatrix();
+}
+
+void draw_eyes()
+{
+  glPushMatrix();
+    // Draw the left eye
+    set_color_green();
+    glTranslatef(-0.15, 0.0, 0.36);
+    glScalef(1.0, 1.0, 0.0);
+    glutSolidSphere(0.1, 10, 10);
+    set_color_default();
+  glPopMatrix();
+  glPushMatrix();
+    // Draw the right eye
+    set_color_green();
+    glTranslatef(0.15, 0.0, 0.36);
+    glScalef(1.0, 1.0, 0.0);
+    glutSolidSphere(0.1, 10, 10);
+    set_color_default();
+  glPopMatrix();
+}
+
+void draw_head()
+{
+  // Draw the head, eyes, and antenna
+  glPushMatrix();
+    glTranslatef(0.0, 1.2, 0.0);
+    glPushMatrix();
+      set_color_blue();
+      glScalef(1.0, 0.8, 1.0);
+      glutSolidCube(0.7);
+      set_color_default();
+    glPopMatrix();
+    draw_eyes();
+    draw_antenna();
+  glPopMatrix();
+}
+
 void draw_robot(void)
 {
   // Start drawing the robot
-  glColor3f(1.0, 0.0, 0.0);
+  set_color_default();
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 
@@ -45,75 +157,11 @@ void draw_robot(void)
   glRotatef(y_rotate, 0.0, 1.0, 0.0);
   glRotatef(x_rotate, 1.0, 0.0, 0.0);
 
-  // Draw the main body
-  glPushMatrix();
-    glScalef(1.0, 1.5, 1.0);
-    glutSolidCube(1.0);
-  glPopMatrix();
-
-  // Draw rectangle on front of  body
-  glPushMatrix();
-    glColor3f(0.0, 1.0, 0.0);
-    glTranslatef(0.0, 0.0, 0.51);
-    glScalef(0.5, 1.0, 0.0);
-    glutSolidCube(1.0);
-    glColor3f(1.0, 0.0, 0.0);
-  glPopMatrix();
-
-  // Draw two triangles on back of body
-  glPushMatrix();
-    glColor3f(0.0, 1.0, 0.0);
-    glTranslatef(0.0, 0.0, -0.51);
-    glPushMatrix();
-      glTranslatef(0.0, 0.0, 0.0);
-      draw_triangle(1.0);
-    glPopMatrix();
-    glPushMatrix();
-      glTranslatef(0.0, -0.5, 0.0);
-      draw_triangle(1.0);
-    glPopMatrix();
-    glColor3f(1.0, 0.0, 0.0);
-  glPopMatrix();
-
-  // Draw the neck
-  glPushMatrix();
-    glColor3f(0.0, 1.0, 0.0);
-    glTranslatef(0.0, 0.9, 0.0);
-    glRotatef(90, 1.0, 0.0, 0.0);
-    GLUquadricObj *quadratic;
-    quadratic = gluNewQuadric();
-    gluCylinder(quadratic, 0.3, 0.3, 0.2, 32, 32);
-    glColor3f(1.0, 0.0, 0.0);
-  glPopMatrix();
-
-  // Draw the head
-  glPushMatrix();
-    glTranslatef(0.0, 1.2, 0.0);
-    glPushMatrix();
-      // The head isn't quite a cube
-      glScalef(1.0, 0.8, 1.0);
-      glutWireCube(0.7);
-    glPopMatrix();
-    glPushMatrix();
-      // Draw the left eye
-      glTranslatef(-0.15, 0.0, 0.35);
-      glScalef(1.0, 1.0, 0.0);
-      glutSolidSphere(0.1, 10, 10);
-    glPopMatrix();
-    glPushMatrix();
-      // Draw the right eye
-      glTranslatef(0.15, 0.0, 0.35);
-      glScalef(1.0, 1.0, 0.0);
-      glutSolidSphere(0.1, 10, 10);
-    glPopMatrix();
-    glPushMatrix();
-      // Draw the antenna
-      glTranslatef(0.0, 0.65, 0.0);
-      glRotatef(90, 1.0, 0.0, 0.0);
-      quadratic = gluNewQuadric();
-      gluCylinder(quadratic, 0.1, 0.1, 0.4, 32, 32);
-    glPopMatrix();
-  glPopMatrix();
+  draw_body();
+  draw_front_rectangle();
+  draw_rear_triangles();
+  draw_neck();
+  draw_head();
 }
 
 void display(void)
@@ -121,9 +169,7 @@ void display(void)
   glClearColor(1.0, 1.0, 1.0, 0.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   gluLookAt(0.1,0.1,0.5,0.0,0.0,0.0,0.0,1.0,0.0);
-
   draw_robot();
-
   glutSwapBuffers();
 }
 
